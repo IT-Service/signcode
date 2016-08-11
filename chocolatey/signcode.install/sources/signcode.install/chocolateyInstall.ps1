1f28b60f6960148cfb5151f4c7880984014b8f8d
sergey.s.betke <sergey.s.betke@yandex.ru>
2016-08-11 20:37:20 +0300

    - заменены контрольные суммы внешних файлов на корректные (#16) + добавлена проверка контрольных сумм внешних файлов при установке (#16)

--- chocolatey/signcode.install/sources/signcode.install/chocolateyInstall.ps1
+++ chocolatey/signcode.install/sources/signcode.install/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
+$fileHashAlgorithm = 'md5';
 
 $packageName = 'signcode.install';
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition);
@@ -6,16 +7,28 @@ $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition);
 $DSigUrl = 'http://download.microsoft.com/download/4/7/e/47e8e7fb-9441-4887-988f-e259a443052d/Dsig.EXE';
 $signcodePwdUrl = 'http://www.stephan-brenner.com/downloads/signcode-pwd/signcode-pwd_1_02.zip';
 
+$dsigMetadata = ( Select-Xml `
+    -LiteralPath ( Join-Path -Path $toolsDir -ChildPath 'dsig.externalfile.xml' ) `
+    -XPath '/package/files/file[@fileid="dsig"]'
+).Node;
 Install-ChocolateyPackage `
     -packageName 'DSig' `
-    -url $DSigUrl `
+    -url ( $dsigMetadata.url ) `
     -silentArgs "/Q /T:`"$toolsDir`"" `
+    -checksum ( Get-Content -LiteralPath ( Join-Path -Path $toolsDir -ChildPath "dsig.$fileHashAlgorithm.checksum.txt" ) ) `
+    -checksumType $fileHashAlgorithm `
 ;
 
+$signcodepwdMetadata = ( Select-Xml `
+    -LiteralPath ( Join-Path -Path $toolsDir -ChildPath 'signcodepwd.externalfile.xml' ) `
+    -XPath '/package/files/file[@fileid="signcodepwd"]'
+).Node;
 Install-ChocolateyZipPackage `
     -packageName 'signcode-pwd' `
     -unzipLocation $toolsDir `
-    -url $signcodePwdUrl `
+    -url ( $signcodepwdMetadata.url ) `
+    -checksum ( Get-Content -LiteralPath ( Join-Path -Path $toolsDir -ChildPath "signcodepwd.$fileHashAlgorithm.checksum.txt" ) ) `
+    -checksumType $fileHashAlgorithm `
 ;
 
 $exitCode = Start-ChocolateyProcessAsAdmin `
